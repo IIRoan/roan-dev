db1cddbefbf6a73e38235c74400a9279faf1c3e4
IIRoan <github.com.q6c0y@lunary.roan.zip>
2025-11-10 20:34:03 +0100

    Added analytics and speed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +23,8 @@ export default function RootLayout({
       >
         {children}
       </body>
+      <Analytics />
+      <SpeedInsights />
     </html>
   );
 }
